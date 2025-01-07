7e225a36993ef7a2ff0ec58fb017bfc81f7a90d6
oliveboss <160532421+oliveboss@users.noreply.github.com>
2025-01-07 00:45:11 -0500

    Add files via upload

---
+++ SolutionGestionConsultationsContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolutionGestionConsultations.Core.Entities;
+
+namespace SolutionGestionConsultations.Infrastructure
+{
+    public class SolutionGestionConsultationsContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Consultation> Consultations { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<DossierMédical> DossiersMédicals { get; set; }
+
+        // Constructeur avec options pour le contexte
+        public SolutionGestionConsultationsContext(DbContextOptions<SolutionGestionConsultationsContext> options)
+            : base(options)
+        { }
+
+        // Constructeur pour configurer la chaîne de connexion à SQL Server
+        public SolutionGestionConsultationsContext() : base(new DbContextOptionsBuilder<SolutionGestionConsultationsContext>()
+            .UseSqlServer(@"Server=OLIVE_COMPUTER\sqlexpress;Database=GestionConsultations;Integrated Security=True;TrustServerCertificate=True;")
+            .Options)
+        { }
+    }
+}
---
+++ SolutionGestionConsultationsContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace SolutionGestionConsultations.Infrastructure
+{
+    public class SolutionGestionConsultationsContextFactory : IDesignTimeDbContextFactory<SolutionGestionConsultationsContext>
+    {
+        public SolutionGestionConsultationsContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SolutionGestionConsultationsContext>();
+            optionsBuilder.UseSqlServer(@"Server=OLIVE_COMPUTER\sqlexpress;Database=GestionConsultations;Integrated Security=True;TrustServerCertificate=True;");
+            return new SolutionGestionConsultationsContext(optionsBuilder.Options);
+        }
+    }
+}
